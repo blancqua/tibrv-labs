e56bda22c3cee095da62fca7cd6490613c24bb63
Wouter Blancquaert <wouter.blancquaert@gmail.com>
2013-12-10 01:19:11 -0800

    Create tests for Lab B and C

---
+++ src/test/java/com/tibco/tibrv/labs/labb/OpenRVTest.java
@@ -0,0 +1,13 @@
+package com.tibco.tibrv.labs.labb;
+
+import org.junit.Test;
+
+
+public class OpenRVTest {
+
+	@Test
+	public void openRv() throws Exception {
+		OpenRV.main(new String[] {});
+	}
+	
+}
---
+++ src/test/java/com/tibco/tibrv/labs/labc/SendToRvTest.java
@@ -0,0 +1,12 @@
+package com.tibco.tibrv.labs.labc;
+
+import org.junit.Test;
+
+public class SendToRvTest {
+
+	@Test
+	public void sendToRv() throws Exception {
+		SendToRv.main(new String[] { "Hello", "Hello!" });
+	}
+	
+}
